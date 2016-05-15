19c65eee637b6ce69c0d731824c28a6b7518fad9
James Mead <james@floehopper.org>
2016-05-15 15:48:11 +0100

    Further improvements to spec coverage.

--- spec/fast_fourier_transform_spec.rb
+++ spec/fast_fourier_transform_spec.rb
@@ -1,6 +1,20 @@
 require 'fast_fourier_transform'
 
 RSpec.describe FastFourierTransform do
+  it 'gives same transform as DFT' do
+    original_signal = [0, 1, 0, 1, 0, 1]
+    dft_transformed_signal = DiscreteFourierTransform.transform(original_signal)
+    fft_transformed_signal = FastFourierTransform.transform(original_signal)
+    expect(fft_transformed_signal.map(&:real).map(&:round)).to eq (dft_transformed_signal.map(&:real).map(&:round))
+  end
+
+  it 'gives same inverse transform as DFT' do
+    transformed_signal = [3, 0, 0, -3, 0, 0]
+    dft_signal = DiscreteFourierTransform.inverse_transform(transformed_signal)
+    fft_signal = FastFourierTransform.inverse_transform(transformed_signal)
+    expect(dft_signal.map(&:real).map(&:round)).to eq (dft_signal.map(&:real).map(&:round))
+  end
+
   it 'roundtrips input with even number of values' do
     original_signal = [0, 1, 0, 1, 0, 1]
     transformed_signal = FastFourierTransform.transform(original_signal)
